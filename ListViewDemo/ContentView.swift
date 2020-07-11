62de3e0322314b76e16aed523ffec55371347612
Mohan Chanddrasekar <psgtech.mohan@gmail.com>
2020-07-11 15:12:57 +0200

    Initial Commit

---
+++ ListViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListViewDemo
+//
+//  Created by Mohan Chandrasekar  on 2020-07-11.
+//  Copyright © 2020 Mohan Chandrasekar . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
